c96bc134873a5775e63bfe2f726b7d62b62e0be7
semenikhin <garry@nextmail.ru>
2022-12-21 23:15:41 +0300

    Задача 1

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,19 @@
 public class Main {
     public static void main(String[] args) {
 
+        System.out.println("Задача 1");
+        byte mouse = 100;
+        System.out.println("Значение переменной mouse с типом byte равно " + mouse);
+        short dog = 10000;
+        System.out.println("Значение переменной dog с типом short равно " + dog);
+        int bull = 1000000000;
+        System.out.println("Значение переменной bull с типом int равно " + bull);
+        long elephant = 1000000000000000000L;
+        System.out.println("Значение переменной elephant с типом long равно " + elephant);
+        float dolphin = 0.000001f;
+        System.out.println("Значение переменной dolphin с типом float равно " + dolphin);
+        double whale = 0.0000000000000001;
+        System.out.println("Значение переменной whale с типом double равно " + whale);
+
     }
 }
